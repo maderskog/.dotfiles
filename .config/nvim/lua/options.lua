e37d8ae6331929fe025605e6dceb3c1f4066404d
Michael Aderskog <michael.aderskog@kontali.no>
2024-07-21 20:07:06 +0200

    Try to get blinking cursor in nvim

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,3 +4,9 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.guicursor = {
+    "n-v-c:block-nCursor/lCursor-blinkwait0-blinkon10-blinkoff100",
+    "i-ci:ver25-nCursor/lCursor-blinkwait0-blinkon10-blinkoff100",
+    "r:hor50-nCursor/lCursor-blinkwait0-blinkon10-blinkoff100"
+}
