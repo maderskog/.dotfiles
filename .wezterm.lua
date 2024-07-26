4baa6f40ed312115b7cbc6dc12e82f28ac37ec0f
Michael Aderskog <michael.aderskog@kontali.no>
2024-07-26 23:18:26 +0200

    Adjust cursor and font

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,9 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.default_cursor_style = 'BlinkingBlock'
+config.default_cursor_style = 'BlinkingBar'
 config.use_fancy_tab_bar = false
+config.font_size = 19.0
+config.cursor_thickness = 4
 
 return config
