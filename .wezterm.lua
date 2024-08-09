6e975b85f330e53136345e9c8dd17a1d019d1022
Michael Aderskog <michael.aderskog@kontali.no>
2024-08-09 16:43:43 +0200

    Change cursor

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,9 +2,10 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.default_cursor_style = 'BlinkingBar'
+config.default_cursor_style = 'BlinkingUnderline'
 config.use_fancy_tab_bar = false
 config.font_size = 19.0
 config.cursor_thickness = 4
+config.window_background_opacity = 0.9
 
 return config
