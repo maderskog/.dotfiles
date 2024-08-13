38f287644a3d6fcf1ae09aa252c407750cd1eff0
Michael Aderskog <michael.aderskog@kontali.no>
2024-08-13 22:26:21 +0200

    Minor adjustments

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,8 +4,8 @@ local config = wezterm.config_builder()
 
 config.default_cursor_style = 'BlinkingUnderline'
 config.use_fancy_tab_bar = false
-config.font_size = 19.0
-config.cursor_thickness = 4
-config.window_background_opacity = 0.9
+config.font_size = 18.0
+config.cursor_thickness = 3
+config.window_background_opacity = 0.87
 
 return config
