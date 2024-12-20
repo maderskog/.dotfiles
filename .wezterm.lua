befd8c2b8137ca8c6b857ea928bd36428fef312f
Michael Aderskog <michael.aderskog@kontali.no>
2024-12-20 11:17:54 -0600

    change caret to block

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.default_cursor_style = 'BlinkingUnderline'
+config.default_cursor_style = 'BlinkingBlock'
 config.use_fancy_tab_bar = false
 config.font_size = 18.0
 config.cursor_thickness = 3
