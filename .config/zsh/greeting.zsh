efff088facf191f5651b259436fcacfdf71e0246
Michael Aderskog <83700978+maderskog@users.noreply.github.com>
2025-11-17 23:49:32 +0100

    Add fastfetch as greeting

--- .config/zsh/greeting.zsh
+++ .config/zsh/greeting.zsh
@@ -2,3 +2,6 @@ if [[ "$TERM_PROGRAM" == "ghostty" ]]; then
   ghostcolors=( "brightred" "brightgreen" "brightyellow" "brightblue" "brightmagenta" "brightcyan" "brightwhite")
   ghosttime --color ${ghostcolors[$(($RANDOM % ${#ghostcolors[@]} + 1))]}
 fi
+if [[ -n "$(command -v fastfetch)" ]]; then
+  fastfetch
+fi
