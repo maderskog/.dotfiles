96688176645e58b1b27a97c23e46505584dd38d7
Michael Aderskog <83700978+maderskog@users.noreply.github.com>
2026-01-20 23:27:14 +0100

    Add global and suffix aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -10,4 +10,29 @@ alias l="lsd --long --classify --group-directories-first"
 alias la="lsd --long --almost-all --classify --group-directories-first"
 alias tree="tree -C -a -I .git"
 alias vi='nvim'
-alias json='fx'
+
+# Suffix aliases
+alias -s json='jless'
+alias -s yaml='jless'
+alias -s md='glow -t'
+
+# Global aliases
+
+# No Errors
+alias -g NE='2>/dev/null'
+
+# No Output
+alias -g NO='>/dev/null'
+
+# Redirect both stdout and stderr to /dev/null
+alias -g NUL='>/dev/null 2>&1'
+
+# Pipe to jq
+alias -g J='| jq'
+
+# Pipe to fx
+alias -g F='| fx'
+
+# Copy output to clipboard (macOS)
+alias -g C='| pbcopy'
+
