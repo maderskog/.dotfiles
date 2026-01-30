a223d297d3981c58c1c50a4a3db1cc093f9059e2
Michael Aderskog <83700978+maderskog@users.noreply.github.com>
2026-01-30 20:14:06 +0100

    Update oh-my-posh theme and zsh keybindings
    Changed arrow and Python icons in the oh-my-posh zen.yaml theme, adjusted folder block options, and tweaked some template spacing. Updated zsh keybindings to use '^Xvi' for edit-command-line and added a newline for readability.

--- home/dot-config/zsh/keybindings.zsh
+++ home/dot-config/zsh/keybindings.zsh
@@ -1,6 +1,7 @@
 autoload -Uz edit-command-line
 zle -N edit-command-line
-bindkey '^X^E' edit-command-line
+bindkey '^Xvi' edit-command-line
 
 bindkey ' ' magic-space
+
 bindkey -s '^Xgc' 'git commit -m ""\C-b'
